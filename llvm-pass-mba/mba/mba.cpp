d059480367893d05e7a0556f292abf99d7e8c09b
Roman Oravec <romanoravec1@gmail.com>
2021-07-20 22:31:17 +0200

    Added more substitutions, refactoring

--- llvm-pass-mba/mba/mba.cpp
+++ llvm-pass-mba/mba/mba.cpp
@@ -8,6 +8,11 @@
 
 using namespace llvm;
 
+static cl::opt<int>
+ObfProb("sub_prob",
+         cl::desc("Choose the probability of obfuscating a binary operation"),
+         cl::value_desc("probability"), cl::init(100),  cl::Optional);
+
 namespace {
   struct MbaPass : public BasicBlockPass {
     
@@ -15,7 +20,14 @@ namespace {
 
     MbaPass() : BasicBlockPass(ID) {}
 
+    Value *SubAdd(BinaryOperator *BinOp);
+    Value *SubSub(BinaryOperator *BinOp);
+    Value *SubXor(BinaryOperator *BinOp);
+    Value *SubAnd(BinaryOperator *BinOp);
+    Value *SubOr(BinaryOperator *BinOp);
+
     virtual bool runOnBasicBlock(BasicBlock &BB) {
+      
       bool changed = false;
       for (auto current = BB.begin(), last = BB.end(); current != last; current++){
         Instruction &Inst = *current;
@@ -28,7 +40,7 @@ namespace {
         // Maybe insert probability of replacement (condition) here
 
         unsigned Opcode = BinOp->getOpcode();
-        // Substitute only integer ops
+        // Substitute only integer operations
         if (!BinOp->getType()->isIntegerTy())
           continue;
         
@@ -36,61 +48,112 @@ namespace {
         Value *NewValue;
 
         switch(Opcode){
-          case Instruction::Sub: 
-            // Substitute x - y --> 2*(x & ~y) - (x ^ y)
-            NewValue = Builder.CreateSub(
-              Builder.CreateMul(
-                ConstantInt::get(BinOp->getType(), 2),
-                Builder.CreateAnd(BinOp->getOperand(0),
-                  Builder.CreateNot(BinOp->getOperand(1)))
-              ),
-              Builder.CreateXor(BinOp->getOperand(0), BinOp->getOperand(1))      
-            );
-            break;
-
           case Instruction::Add:
-            // Substitute x + y --> 2*(x | y) - (x ^ y)
-            NewValue = Builder.CreateSub(
-              Builder.CreateMul(
-                ConstantInt::get(BinOp->getType(), 2),
-                Builder.CreateOr(BinOp->getOperand(0), BinOp->getOperand(1))
-              ),
-              Builder.CreateXor(BinOp->getOperand(0), BinOp->getOperand(1))      
-            );
+            ReplaceInstWithValue(BB.getInstList(), current, SubAdd(BinOp));
             break;
-
+          case Instruction::Sub: 
+            ReplaceInstWithValue(BB.getInstList(), current, SubSub(BinOp));
+            break;
           case Instruction::Xor:
-            // Substitute x ^ y --> (x | y) - (x & y)
-            NewValue = Builder.CreateSub(
-              Builder.CreateOr(BinOp->getOperand(0), BinOp->getOperand(1)),
-              Builder.CreateAnd(BinOp->getOperand(0), BinOp->getOperand(1))      
-            );
+            ReplaceInstWithValue(BB.getInstList(), current, SubXor(BinOp));
             break;
-
           case Instruction::And:
-            // Substitute x & y --> (~x | y) - ~x
-            NewValue = Builder.CreateSub(
-              Builder.CreateOr(
-                Builder.CreateNot(BinOp->getOperand(0)),
-                BinOp->getOperand(1)
-              ),
-              Builder.CreateNot(BinOp->getOperand(0))
-            );
+            ReplaceInstWithValue(BB.getInstList(), current, SubAnd(BinOp));
+            break;
+          case Instruction::Or:
+            ReplaceInstWithValue(BB.getInstList(), current, SubOr(BinOp));
+            break;
           default: continue;
         }
-
-        // Replace the old instruction
-        ReplaceInstWithValue(BB.getInstList(), current, NewValue);
-
         changed = true;
-      }
-
+      } // for loop
       return changed;
-    }
-  };
+    } // runOnBasicBlock
+  }; // MbaPass
+
+} // namespace
+
+// Substitute x + y --> 2*(x | y) - (x ^ y)
+Value* MbaPass::SubAdd(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateMul(
+      ConstantInt::get(BinOp->getType(), 2),
+      Builder.CreateOr(
+        BinOp->getOperand(0), 
+        BinOp->getOperand(1))
+    ),
+    Builder.CreateXor(
+      BinOp->getOperand(0), 
+      BinOp->getOperand(1))      
+  );
+  return NewValue;
+}
+
+// Substitute x - y --> 2*(x & ~y) - (x ^ y)
+Value *MbaPass::SubSub(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateMul(
+      ConstantInt::get(BinOp->getType(), 2),
+      Builder.CreateAnd(
+        BinOp->getOperand(0),
+        Builder.CreateNot(BinOp->getOperand(1)))
+    ),
+    Builder.CreateXor(
+      BinOp->getOperand(0), 
+      BinOp->getOperand(1))  
+  );
+  return NewValue;
+}
+
+// Substitute x ^ y --> (x | y) - (x & y)
+Value *MbaPass::SubXor(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateOr(
+      BinOp->getOperand(0), 
+      BinOp->getOperand(1)),
+    Builder.CreateAnd(
+      BinOp->getOperand(0), 
+      BinOp->getOperand(1))      
+  );
+  return NewValue;
+}
+
+// Substitute x & y --> (~x | y) - ~x
+Value *MbaPass::SubAnd(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateOr(
+      Builder.CreateNot(
+        BinOp->getOperand(0)),
+        BinOp->getOperand(1)
+    ),
+    Builder.CreateNot(BinOp->getOperand(0))
+  );
+  return NewValue;
+}
+
+// Substitute x | y --> (x & ~y) + y
+Value *MbaPass::SubOr(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateAdd(
+    Builder.CreateAnd(
+      BinOp->getOperand(0),
+      Builder.CreateNot(BinOp->getOperand(1))
+    ),
+    BinOp->getOperand(1)
+  );
+  return NewValue;
 }
 
 char MbaPass::ID = 0;
 
 // Register the pass 
-static RegisterPass<MbaPass> X("mba", "Substitute instructions with MBA");
+static RegisterPass<MbaPass> X("mba", "Substitute binary operations with MBA");
