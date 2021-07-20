a05589eee1606cb19d8bf553c50bbe4a5dda0e75
Roman Oravec <romanoravec1@gmail.com>
2021-07-20 22:51:52 +0200

    delete test codes

--- llvm-pass-mba/add.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]){
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    int c = a + b;
-    printf("The result is %d\n", c);
-}
--- llvm-pass-mba/example.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, const char** argv) {
-	int num;
-	scanf("%i", &num);
-	printf("%i\n", num + 2);
-	return 0;
-}
