fb5493c8e5047a7db3523116437dc8c0f0c707d2
Roman Oravec <romanoravec1@gmail.com>
2021-09-23 21:52:41 +0200

    fixed pointer type in createDecodeStubFunc

--- llvm-pass-obfstring/obfstring/obfstring.cpp
+++ llvm-pass-obfstring/obfstring/obfstring.cpp
@@ -6,6 +6,8 @@
 #include "llvm/IR/ValueSymbolTable.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Linker/IRMover.h"
 #include <typeinfo>
 
 #define STRING_ENCRYPTION_KEY "S5zeMZ68*K7ddKwiOWTt"
@@ -13,65 +15,102 @@
 using namespace std;
 using namespace llvm;
 
-namespace {
-class GlobalString {
+namespace
+{
+	class GlobalString
+	{
 	public:
-		GlobalVariable* Glob;
+		GlobalVariable *Glob;
 		unsigned int index;
 		int type;
 		int string_length;
 		static const int SIMPLE_STRING_TYPE = 1;
 		static const int STRUCT_STRING_TYPE = 2;
 
-		GlobalString(GlobalVariable* Glob, int length) : Glob(Glob), index(-1), string_length(length), type(SIMPLE_STRING_TYPE) {}
-		GlobalString(GlobalVariable* Glob, unsigned int index, int length) : Glob(Glob), index(index), string_length(length), type(STRUCT_STRING_TYPE) {}
-};
+		GlobalString(GlobalVariable *Glob, int length) : Glob(Glob), index(-1), string_length(length), type(SIMPLE_STRING_TYPE) {}
+		GlobalString(GlobalVariable *Glob, unsigned int index, int length) : Glob(Glob), index(index), string_length(length), type(STRUCT_STRING_TYPE) {}
+	};
 
-Function *createDecodeStubFunc(Module &M, vector<GlobalString*> &GlobalStrings, Function *DecodeFunc){
-	auto &Ctx = M.getContext();
-	// Add DecodeStub function
-	FunctionCallee DecodeStubCallee = M.getOrInsertFunction("decode_stub",
-/*ret*/		Type::getVoidTy(Ctx));
-	Function *DecodeStubFunc = cast<Function>(DecodeStubCallee.getCallee());
-	DecodeStubFunc->setCallingConv(CallingConv::C);
+	Function *createDecodeStubFunc(Module &M, vector<GlobalString *> &GlobalStrings, Function *DecodeFunc)
+	{
+		auto &Ctx = M.getContext();
+		// Add DecodeStub function
+		FunctionCallee DecodeStubCallee = M.getOrInsertFunction("decode_stub",
+																/*ret*/ Type::getVoidTy(Ctx));
+		Function *DecodeStubFunc = cast<Function>(DecodeStubCallee.getCallee());
+		DecodeStubFunc->setCallingConv(CallingConv::C);
 
-	// Create entry block
-	BasicBlock *BB = BasicBlock::Create(Ctx, "entry", DecodeStubFunc);
-	IRBuilder<> Builder(BB);
+		// Create entry block
+		BasicBlock *BB = BasicBlock::Create(Ctx, "entry", DecodeStubFunc);
+		IRBuilder<> Builder(BB);
 
-	// Add calls to decode every encoded global
-	for(GlobalString *GlobString : GlobalStrings){
-		if(GlobString->type == GlobString->SIMPLE_STRING_TYPE){
-			auto *StrPtr = Builder.CreatePointerCast(GlobString->Glob, Type::getInt8PtrTy(Ctx, 8));
-			llvm::Value *le = llvm::ConstantInt::get(
-			llvm::IntegerType::getInt32Ty(Ctx), GlobString->string_length);			
-			Builder.CreateCall(DecodeFunc, {StrPtr, le});
+		// Add calls to decode every encoded global
+		for (GlobalString *GlobString : GlobalStrings)
+		{
+			if (GlobString->type == GlobString->SIMPLE_STRING_TYPE)
+			{
+				auto *StrPtr = Builder.CreatePointerCast(GlobString->Glob, Type::getInt8PtrTy(Ctx/* , 8 */));
+				/* llvm::Value *le = llvm::ConstantInt::get(
+					llvm::IntegerType::getInt32Ty(Ctx), GlobString->string_length); */
+				Builder.CreateCall(DecodeFunc, {StrPtr}); //CHANGE: removed le
+			}
+			else if (GlobString->type == GlobString->STRUCT_STRING_TYPE)
+			{
+				auto *String = Builder.CreateStructGEP(GlobString->Glob, GlobString->index);
+				auto *StrPtr = Builder.CreatePointerCast(String, Type::getInt8PtrTy(Ctx/* , 8 */));
+				/* llvm::Value *le = llvm::ConstantInt::get(
+					llvm::IntegerType::getInt32Ty(Ctx), GlobString->string_length); */
+				Builder.CreateCall(DecodeFunc, {StrPtr}); //CHANGE: removed le
+			}
 		}
-		else if(GlobString->type == GlobString->STRUCT_STRING_TYPE){
-			auto *String = Builder.CreateStructGEP(GlobString->Glob, GlobString->index);
-			auto *StrPtr = Builder.CreatePointerCast(String, Type::getInt8PtrTy(Ctx, 8));
-		 	llvm::Value *le = llvm::ConstantInt::get(
-			llvm::IntegerType::getInt32Ty(Ctx), GlobString->string_length);
-			Builder.CreateCall(DecodeFunc, {StrPtr, le});
-		}
+		Builder.CreateRetVoid();
+
+		return DecodeStubFunc;
 	}
-	Builder.CreateRetVoid();
 
-	return DecodeStubFunc;
-}
+	//TODO: use llvm::parseIRFile()
+	Function *createDecodeFunc(Module &M)
+	{
+		auto &Ctx = M.getContext();
+		llvm::SMDiagnostic mod_err;
+		// TODO: use flag instead of hardcoded path
+		unique_ptr<Module> DecModule = parseIRFile("decode.bc", mod_err, Ctx);
+		if (!DecModule)
+			//assert(DecModule && "Unable to parse bitcode file ");
+			mod_err.print("Unable to parse BC file", errs());
+		// Check if we have an decoding function
+		assert(DecModule->getFunction("decode") && "Decoding function not found");
 
-//TODO: use llvm::parseIRFile()
-Function *createDecodeFunc(Module &M){
-	auto &Ctx = M.getContext();
-	FunctionCallee DecodeFuncCallee = M.getOrInsertFunction("decode", 
-	/*ret*/			Type::getVoidTy(Ctx),
- /*args*/		Type::getInt8PtrTy(Ctx, 8),
- 				Type::getInt32Ty(Ctx));
+		std::vector<llvm::GlobalValue *> imports({DecModule->getFunction("decode")});
 
-	Function *DecodeFunc = cast<Function>(DecodeFuncCallee.getCallee());
-	DecodeFunc->setCallingConv(CallingConv::C);
+		auto err = IRMover(M).move(
+			move(DecModule), imports,
+			[](llvm::GlobalValue &GV, llvm::IRMover::ValueAdder Add) {},
+			/*IsPerformingImport=*/false);
+		handleAllErrors(std::move(err), [&](llvm::ErrorInfoBase &eib)
+						{ errs() << "Could not import decoder function: " << eib.message(); });
+		// Set attributes to the new decoder
+		auto DecodeFunc = M.getFunction("decode");
 
-	// Name DecodeFunc arguments
+		/* unsigned Idx = 0;
+		for (auto &Arg : DecodeFunc->args()) */
+			
+
+		DecodeFunc->removeFnAttr(llvm::Attribute::OptimizeNone);
+		DecodeFunc->removeFnAttr(llvm::Attribute::NoInline);
+		DecodeFunc->addFnAttr(llvm::Attribute::AlwaysInline);
+		// Set internal linkage to avoid naming conflicts
+		DecodeFunc->setLinkage(llvm::GlobalValue::LinkageTypes::InternalLinkage);
+
+		//FunctionCallee DecodeFuncCallee = M.getOrInsertFunction("decode",
+		///*ret*/			Type::getVoidTy(Ctx),
+		///*args*/		Type::getInt8PtrTy(Ctx, 8),
+		//			Type::getInt32Ty(Ctx));
+
+		//Function *DecodeFunc = cast<Function>(DecodeFuncCallee.getCallee());
+		DecodeFunc->setCallingConv(CallingConv::C);
+
+		/* // Name DecodeFunc arguments
 	Function::arg_iterator Args = DecodeFunc->arg_begin();
 	Value *var0 = Args++;
 	Value *var1 = Args++;
@@ -108,117 +147,92 @@ Function *createDecodeFunc(Module &M){
 	phi_var7->addIncoming(var10, bb6);
 //   %8 = phi i32 [ %9, %6 ], [ %1, %2 ]
 	phi_var8->addIncoming(var1, bb2);
-	phi_var8->addIncoming(var9, bb6);
+	phi_var8->addIncoming(var9, bb6); */
 
-	return DecodeFunc;
-}
-
-
-void createDecodeStubBlock(Function *F, Function *DecodeStubFunc){
-	auto &Ctx = F->getContext();
-	BasicBlock &EntryBlock = F->getEntryBlock();
-
-	// Create new block
-	BasicBlock *NewBB = BasicBlock::Create(Ctx, "DecodeStub", EntryBlock.getParent(), &EntryBlock);
-	IRBuilder<> Builder(NewBB);
-
-	// Call stub func instruction
-	Builder.CreateCall(DecodeStubFunc);
-	// Jump to original entry block
-	Builder.CreateBr(&EntryBlock);
-}
-
-// used for debug, remove later
-void PrintDecoded(const unsigned char *str, unsigned int len) {
-	int i = 0;
-	unsigned char *decStr = (unsigned char *)calloc(len, sizeof(unsigned char));
-  	for (; str[i] != 0; ++i) {
-    	unsigned key_idx = i % (sizeof(STRING_ENCRYPTION_KEY) - 1);
-    	decStr[i] = str[i]^STRING_ENCRYPTION_KEY[key_idx];
-    	if (decStr[i] == 0xff) {
-      		decStr[i] = STRING_ENCRYPTION_KEY[key_idx];
-    	}
-  	}
-  	errs() << format(" \nDecoded string:%s\n", reinterpret_cast<char *>(decStr));
-  	return;
-}
-
-char *EncodeString(const char* Data, unsigned int Length) {
-	// Encode string
-	unsigned char *NewData = (unsigned char*)calloc(Length, sizeof(unsigned char));
-	int i = 0;
-	for (; Data[i] != 0; ++i) {
-		errs() << format("%c\t%X\t%X\n", Data[i],Data[i], NewData[i]);
-		unsigned key_idx = i % (sizeof(STRING_ENCRYPTION_KEY) - 1);
-		if (Data[i] == 0xff) {
-			// value 0xff is banned in the string because it is used as
-			// a placeholder for 0 in encrypted result
-			NewData[i] = '.';
-		}
-		NewData[i] = Data[i]^STRING_ENCRYPTION_KEY[key_idx];
-		if (NewData[i] == 0) {
-			NewData[i] = STRING_ENCRYPTION_KEY[key_idx] ^ 0xff;
-		}
-	}
-	errs() << format("%c\t%X\t%X\n", Data[i++],Data[i++], NewData[i++]);
-	PrintDecoded(NewData, Length);
-	errs() << "---------------------------------\n";
-	return reinterpret_cast<char *>(NewData);
-}
-
-// Original enc with ROR1:
-/* char *EncodeString(const char* Data, unsigned int Length) {
-	// Encode string
-	char *NewData = (char*)malloc(Length);
-	for(unsigned int i = 0; i < Length; i++){
-		NewData[i] = Data[i] + 1;
+		return DecodeFunc;
 	}
 
-	return NewData;
-} */
+	void createDecodeStubBlock(Function *F, Function *DecodeStubFunc)
+	{
+		auto &Ctx = F->getContext();
+		BasicBlock &EntryBlock = F->getEntryBlock();
 
-vector<GlobalString*> encodeGlobalStrings(Module& M){
-	vector<GlobalString*> GlobalStrings;
-	auto &Ctx = M.getContext();
+		// Create new block
+		BasicBlock *NewBB = BasicBlock::Create(Ctx, "DecodeStub", EntryBlock.getParent(), &EntryBlock);
+		IRBuilder<> Builder(NewBB);
 
-	// Encode all global strings
-	for(GlobalVariable &Glob : M.globals()) {
-		// Ignore external globals & uninitialized globals.
-		if (!Glob.hasInitializer() || Glob.hasExternalLinkage())
-			continue;
+		// Call stub func instruction
+		Builder.CreateCall(DecodeStubFunc);
+		// Jump to original entry block
+		Builder.CreateBr(&EntryBlock);
+	}
 
-		// Unwrap the global variable to receive its value
-		Constant *Initializer = Glob.getInitializer();
+	// used for debug, remove later
+	void PrintDecoded(const unsigned char *str, unsigned int len)
+	{
+		int i = 0;
+		unsigned char *decStr = (unsigned char *)calloc(len, sizeof(unsigned char));
+		for (; str[i] != 0; ++i)
+		{
+			unsigned key_idx = i % (sizeof(STRING_ENCRYPTION_KEY) - 1);
+			decStr[i] = str[i] ^ STRING_ENCRYPTION_KEY[key_idx];
+			if (decStr[i] == 0xff)
+			{
+				decStr[i] = STRING_ENCRYPTION_KEY[key_idx];
+			}
+		}
+		errs() << format(" \nDecoded string:%s\n", reinterpret_cast<char *>(decStr));
+		return;
+	}
 
-		// Check if its a string
-		if (isa<ConstantDataArray>(Initializer)) {
-			auto CDA = cast<ConstantDataArray>(Initializer);
-			if (!CDA->isString())
+	char *EncodeString(const char *Data, unsigned int Length)
+	{
+		// Encode string
+		unsigned char *NewData = (unsigned char *)calloc(Length, sizeof(unsigned char));
+		int i = 0;
+		for (; Data[i] != 0; ++i)
+		{
+			//errs() << format("%c\t%X\t%X\n", Data[i],Data[i], NewData[i]);
+			unsigned key_idx = i % (sizeof(STRING_ENCRYPTION_KEY) - 1);
+			if (Data[i] == 0xff)
+			{
+				// value 0xff is banned in the string because it is used as
+				// a placeholder for 0 in encrypted result
+				NewData[i] = '.';
+			}
+			NewData[i] = Data[i] ^ STRING_ENCRYPTION_KEY[key_idx];
+			if (NewData[i] == 0)
+			{
+				NewData[i] = STRING_ENCRYPTION_KEY[key_idx] ^ 0xff;
+			}
+		}
+		//errs() << format("%c\t%X\t%X\n", Data[i++],Data[i++], NewData[i++]);
+		//PrintDecoded(NewData, Length);
+		//errs() << "---------------------------------\n";
+		return reinterpret_cast<char *>(NewData);
+	}
+
+
+	vector<GlobalString *> encodeGlobalStrings(Module &M)
+	{
+		vector<GlobalString *> GlobalStrings;
+		auto &Ctx = M.getContext();
+
+		// Encode all global strings
+		for (GlobalVariable &Glob : M.globals())
+		{
+			// Ignore external globals & uninitialized globals.
+			if (!Glob.hasInitializer() || Glob.hasExternalLinkage())
 				continue;
 
-			// Extract raw string
-			StringRef StrVal = CDA->getAsString();	
-			const char *Data = StrVal.begin();
-			const int Size = StrVal.size();
+			// Unwrap the global variable to receive its value
+			Constant *Initializer = Glob.getInitializer();
 
-			// Create encoded string variable. Constants are immutable so we must override with a new Constant.
-			char *NewData = EncodeString(Data, Size);
-			Constant *NewConst = ConstantDataArray::getString(Ctx, StringRef(NewData, Size), false);
-
-			// Overwrite the global value
-			Glob.setInitializer(NewConst);
-
-			GlobalStrings.push_back(new GlobalString(&Glob, Size));
-			Glob.setConstant(false);
-		}
-		else if(isa<ConstantStruct>(Initializer)){
-			// Handle structs
-			auto CS = cast<ConstantStruct>(Initializer);
-			if(Initializer->getNumOperands() > 1)
-				continue; // TODO: Fix bug when removing this: "Constant not found in constant table!"
-			for(unsigned int i = 0; i < Initializer->getNumOperands(); i++){
-				auto CDA = dyn_cast<ConstantDataArray>(CS->getOperand(i));
-				if (!CDA || !CDA->isString())
+			// Check if its a string
+			if (isa<ConstantDataArray>(Initializer))
+			{
+				auto CDA = cast<ConstantDataArray>(Initializer);
+				if (!CDA->isString())
 					continue;
 
 				// Extract raw string
@@ -226,53 +240,84 @@ vector<GlobalString*> encodeGlobalStrings(Module& M){
 				const char *Data = StrVal.begin();
 				const int Size = StrVal.size();
 
-				// Create encoded string variable
+				// Create encoded string variable. Constants are immutable so we must override with a new Constant.
 				char *NewData = EncodeString(Data, Size);
 				Constant *NewConst = ConstantDataArray::getString(Ctx, StringRef(NewData, Size), false);
 
-				// Overwrite the struct member
-				CS->setOperand(i, NewConst);
+				// Overwrite the global value
+				Glob.setInitializer(NewConst);
 
-				GlobalStrings.push_back(new GlobalString(&Glob, i, Size));
+				GlobalStrings.push_back(new GlobalString(&Glob, Size));
 				Glob.setConstant(false);
 			}
+			else if (isa<ConstantStruct>(Initializer))
+			{
+				// Handle structs
+				auto CS = cast<ConstantStruct>(Initializer);
+				if (Initializer->getNumOperands() > 1)
+					continue; // TODO: Fix bug when removing this: "Constant not found in constant table!"
+				for (unsigned int i = 0; i < Initializer->getNumOperands(); i++)
+				{
+					auto CDA = dyn_cast<ConstantDataArray>(CS->getOperand(i));
+					if (!CDA || !CDA->isString())
+						continue;
+
+					// Extract raw string
+					StringRef StrVal = CDA->getAsString();
+					const char *Data = StrVal.begin();
+					const int Size = StrVal.size();
+
+					// Create encoded string variable
+					char *NewData = EncodeString(Data, Size);
+					Constant *NewConst = ConstantDataArray::getString(Ctx, StringRef(NewData, Size), false);
+
+					// Overwrite the struct member
+					CS->setOperand(i, NewConst);
+
+					GlobalStrings.push_back(new GlobalString(&Glob, i, Size));
+					Glob.setConstant(false);
+				}
+			}
 		}
+
+		return GlobalStrings;
 	}
 
-	return GlobalStrings;
-}
+	struct ObfStringPass : public PassInfoMixin<ObfStringPass>
+	{
+		PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM)
+		{
+			// Transform the strings
+			auto GlobalStrings = encodeGlobalStrings(M);
 
-struct ObfStringPass : public PassInfoMixin<ObfStringPass> {
-	PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM) {
-	// Transform the strings
-	auto GlobalStrings = encodeGlobalStrings(M);
+			// Inject functions
+			Function *DecodeFunc = createDecodeFunc(M);
+			Function *DecodeStub = createDecodeStubFunc(M, GlobalStrings, DecodeFunc);
 
-	// Inject functions
-	Function *DecodeFunc = createDecodeFunc(M);
-	Function *DecodeStub = createDecodeStubFunc(M, GlobalStrings, DecodeFunc);
+			// Inject a call to DecodeStub from main
+			Function *MainFunc = M.getFunction("main");
+			createDecodeStubBlock(MainFunc, DecodeStub);
 
-	// Inject a call to DecodeStub from main
-	Function *MainFunc = M.getFunction("main");
-	createDecodeStubBlock(MainFunc, DecodeStub);
-
-	return PreservedAnalyses::all();
+			return PreservedAnalyses::all();
+		};
 	};
-};
 } // end anonymous namespace
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
-llvmGetPassPluginInfo() {
+llvmGetPassPluginInfo()
+{
 	return {
-		LLVM_PLUGIN_API_VERSION, "ObfStringPass", "v0.1", [](PassBuilder &PB) {
+		LLVM_PLUGIN_API_VERSION, "ObfStringPass", "v0.1", [](PassBuilder &PB)
+		{
 			PB.registerPipelineParsingCallback([](StringRef Name, ModulePassManager &MPM,
-					 ArrayRef<PassBuilder::PipelineElement>) {
-					if(Name == "obfstring"){
-						MPM.addPass(ObfStringPass());
-						return true;
-					}
-					return false;
-				}
-			);
-		}
-	};
+												  ArrayRef<PassBuilder::PipelineElement>)
+											   {
+												   if (Name == "obfstring")
+												   {
+													   MPM.addPass(ObfStringPass());
+													   return true;
+												   }
+												   return false;
+											   });
+		}};
 }
--- llvm-pass-obfstring/rebuild_n_run.sh
+++ llvm-pass-obfstring/rebuild_n_run.sh
@@ -5,7 +5,8 @@ cmake ..
 make
 cd ..
 clang -S -emit-llvm -Xclang -disable-O0-optnone $1.c -o $1.ll
-opt -mem2reg -instnamer -S $1.ll -o $1.ll
+clang -S -emit-llvm $1.c -o $1.ll
+#opt -mem2reg -instnamer -S $1.ll -o $1.ll
 opt -load-pass-plugin=build/obfstring/libObfStringPass.so -passes="obfstring" -S $1.ll -o $1_obfstring.ll
-#clang $1_obfstring.ll -o $1_obfstring
+clang $1_obfstring.ll -o $1_obfstring
 #./$1_obfstring 
