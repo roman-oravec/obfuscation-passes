edff33d32954694d84975387929a0c2a42f56b74
Roman Oravec <romanoravec1@gmail.com>
2021-11-18 21:13:11 +0100

    New subs in MBA pass, 3 for each BinOp

--- llvm-pass-mba/mba/mba.cpp
+++ llvm-pass-mba/mba/mba.cpp
@@ -36,8 +36,16 @@ namespace {
     Value *SubSub3(BinaryOperator *BinOp);
 
     Value *SubXor(BinaryOperator *BinOp);
+    Value *SubXor2(BinaryOperator *BinOp);
+    Value *SubXor3(BinaryOperator *BinOp);
+
     Value *SubAnd(BinaryOperator *BinOp);
+    Value *SubAnd2(BinaryOperator *BinOp);
+    Value *SubAnd3(BinaryOperator *BinOp);
+
     Value *SubOr(BinaryOperator *BinOp);
+    Value *SubOr2(BinaryOperator *BinOp);
+    Value *SubOr3(BinaryOperator *BinOp);
 
     virtual bool runOnBasicBlock(BasicBlock &BB) {
       auto rng = BB.getParent()->getParent()->createRNG(this);
@@ -94,17 +102,47 @@ namespace {
             changed = true;
             break;
           case Instruction::Xor:
-            ReplaceInstWithValue(BB.getInstList(), current, SubXor(BinOp));
+            switch (randNum % 3){
+            case 0:
+              ReplaceInstWithValue(BB.getInstList(), current, SubXor(BinOp));
+              break;
+            case 1:
+              ReplaceInstWithValue(BB.getInstList(), current, SubXor2(BinOp));
+              break;
+            case 2:
+              ReplaceInstWithValue(BB.getInstList(), current, SubXor3(BinOp));
+              break;
+            }
             ++MBACount;
             changed = true;
             break;
           case Instruction::And:
-            ReplaceInstWithValue(BB.getInstList(), current, SubAnd(BinOp));
+            switch (randNum % 3){
+            case 0:
+              ReplaceInstWithValue(BB.getInstList(), current, SubAnd(BinOp));
+              break;
+            case 1:
+              ReplaceInstWithValue(BB.getInstList(), current, SubAnd2(BinOp));
+              break;
+            case 2:
+              ReplaceInstWithValue(BB.getInstList(), current, SubAnd3(BinOp)); 
+              break;
+            }
             ++MBACount;
             changed = true;
             break;
           case Instruction::Or:
-            ReplaceInstWithValue(BB.getInstList(), current, SubOr(BinOp));
+          switch (randNum % 3){
+            case 0:
+              ReplaceInstWithValue(BB.getInstList(), current, SubOr(BinOp));
+              break;
+            case 1:
+              ReplaceInstWithValue(BB.getInstList(), current, SubOr2(BinOp));
+              break;
+            case 2:
+              ReplaceInstWithValue(BB.getInstList(), current, SubOr3(BinOp));
+              break;
+          }
             ++MBACount;
             changed = true;
             break;
@@ -255,6 +293,46 @@ Value *MbaPass::SubXor(BinaryOperator *BinOp){
   return NewValue;
 }
 
+// Substitute x ^ y --> (x | y) − y + (~x & y)
+Value *MbaPass::SubXor2(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateAdd(
+    Builder.CreateSub(
+      Builder.CreateOr(
+        BinOp->getOperand(0),
+        BinOp->getOperand(1)
+      ),
+      BinOp->getOperand(1)
+    ),
+    Builder.CreateAnd(
+      Builder.CreateNot(BinOp->getOperand(0)),
+      BinOp->getOperand(1)
+    )
+  );
+  return NewValue;
+}
+
+// Substitute x ^ y --> (x | y) − (~x | y) + (~x)
+Value *MbaPass::SubXor3(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateAdd(
+    Builder.CreateSub(
+      Builder.CreateOr(
+        BinOp->getOperand(0),
+        BinOp->getOperand(1)
+      ),
+      Builder.CreateOr(
+        Builder.CreateNot(BinOp->getOperand(0)),
+        BinOp->getOperand(1)
+      )
+  ),
+    Builder.CreateNot(BinOp->getOperand(0))
+  );
+  return NewValue;
+}
+
 // Substitute x & y --> (~x | y) - ~x
 Value *MbaPass::SubAnd(BinaryOperator *BinOp){
   Value *NewValue;
@@ -270,6 +348,49 @@ Value *MbaPass::SubAnd(BinaryOperator *BinOp){
   return NewValue;
 }
 
+// Substitute x & y --> (x | y) − (~x & y) − (x & ~y)
+Value *MbaPass::SubAnd2(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateSub(
+      Builder.CreateOr(
+        BinOp->getOperand(0),
+        BinOp->getOperand(1)
+      ),
+      Builder.CreateAnd(
+        Builder.CreateNot(BinOp->getOperand(0)),
+        BinOp->getOperand(1)
+      )
+    ),
+    Builder.CreateAnd(
+      BinOp->getOperand(0),
+      Builder.CreateNot(BinOp->getOperand(1))
+    )
+  );
+  return NewValue;
+}
+
+// Substitute x & y --> (x ^ y) + y + (x & ~y)
+Value *MbaPass::SubAnd3(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateAdd(
+    Builder.CreateAdd(
+      Builder.CreateXor(
+        BinOp->getOperand(0),
+        BinOp->getOperand(1)
+      ),
+      BinOp->getOperand(1)
+    ),
+    Builder.CreateAnd(
+      BinOp->getOperand(0),
+      Builder.CreateNot(BinOp->getOperand(1))
+    )
+  );
+  return NewValue;
+}
+
 // Substitute x | y --> (x & ~y) + y
 Value *MbaPass::SubOr(BinaryOperator *BinOp){
   Value *NewValue;
@@ -280,6 +401,46 @@ Value *MbaPass::SubOr(BinaryOperator *BinOp){
       Builder.CreateNot(BinOp->getOperand(1))
     ),
     BinOp->getOperand(1)
+  );
+  return NewValue;
+}
+
+// Substitute x | y --> (x^y) + y − (~x & y)
+Value *MbaPass::SubOr2(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateAdd(
+      Builder.CreateXor(
+        BinOp->getOperand(0),
+        BinOp->getOperand(1)
+      ),
+      BinOp->getOperand(1)
+    ),
+    Builder.CreateAnd(
+      Builder.CreateNot(BinOp->getOperand(0)),
+      BinOp->getOperand(1)
+    )
+  );
+  return NewValue;
+}
+
+// Substitute x | y --> (x ^ y) + (~x | y) − (~x)
+Value *MbaPass::SubOr3(BinaryOperator *BinOp){
+  Value *NewValue;
+  IRBuilder<> Builder(BinOp);
+  NewValue = Builder.CreateSub(
+    Builder.CreateAdd(
+      Builder.CreateXor(
+        BinOp->getOperand(0),
+        BinOp->getOperand(1)
+      ),
+      Builder.CreateOr(
+        Builder.CreateNot(BinOp->getOperand(0)),
+        BinOp->getOperand(1)
+      )
+    ),
+    Builder.CreateNot(BinOp->getOperand(0))
   );
   return NewValue;
 }
